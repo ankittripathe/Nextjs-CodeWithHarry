2e72b821285b70307765e59cf763cc148165b206
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-02 17:07:17 +0530

    nextjs course

---
+++ 01_setup/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      <h1 className='text-xl'>Problem Solved by Next.js</h1>
+      <ul>
+        <li>Full Stack Solution</li>
+        <li>File Based Routing</li>
+        <li>Additional featues like router from next/navigation</li>
+        <li>Optimized Rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default about
---
+++ 01_setup/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='text-2xl'>I am Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
