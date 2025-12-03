f859cdf29ca981cfd92819efc4bd8da4871f5065
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-03 17:30:28 +0530

    server components

---
+++ 03_serverComp/app/page.js
@@ -0,0 +1,25 @@
+// 'use client'
+import Navbar from "@/components/Navbar";
+import fs from "fs/promises";
+
+export default function Home() {
+  // Yaha BackEnd wala logic likh skte ho, agar aapka server components hai toh.
+  console.log("Hey I am Ankit");
+  let a = fs.readFile(".gitignore");
+  a.then((e) => console.log(e.toString()));
+
+  return (
+    <div>
+      <Navbar/>
+      <h1>I am a Components</h1>
+    </div>
+  );
+}
+
+/*
+Notes: 
+(1) Nextjs me bydefault Sab Server Components hai.
+(2) Server me dikhega jo likhe hai server wala logic. Agar client components hota toh hm log use nhi kr paate..
+(3) Serevr wala answer terminal me dikhega aur console me Server likh ke..
+(4) Maan lo aapko Navbar me hook use krna hai.. jo ke client components hai, toh Shirf Navbar ko use client bana ke serevr components me jake Embedded kr skte hai..
+*/
