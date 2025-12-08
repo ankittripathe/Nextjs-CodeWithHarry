2e96cad9943292dfbe4cf2805bda3df41039c78c
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-08 18:40:21 +0530

    how to use 'use client' in nextjs

--- 02_clientComp/app/page.js
+++ 02_clientComp/app/page.js
@@ -6,13 +6,20 @@ const Home = () => {
   console.log("Run in Console Browser");
 
   return (
-    <div>
+    <div className="text-center">
       <h1 className="text-xl">Count:{count}</h1>
-      <button className="bg-red-600 rounded-md p-1" onClick={() => {setCount(count + 1)}}>
+      <button
+        className="bg-red-600 rounded-md p-1"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
         Increment
       </button>
     </div>
   );
 };
 export default Home;
-// Notes: Next.js me By-default sab Server Components hai. Koie vi hook ko use krne ke liye usko client components bana padta hai. client components banane ke liye client mention krna hota hai at the Top.
+/*
+Next.js me By-default sab Server Components hai. Koie vi hook ko use krne ke liye usko client components bana padta hai. client components banane ke liye 'use client' mention krna hota hai at the top of the code.
+*/
