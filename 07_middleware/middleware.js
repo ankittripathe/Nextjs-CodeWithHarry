5405237a7e9190309e69b7f345235869cb58d1c5
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-09 11:57:39 +0530

    middleware in nextjs

--- 07_middleware/middleware.js
+++ 07_middleware/middleware.js
@@ -2,17 +2,18 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  console.log("Middleware chala");
-  // Condition 1:- displya me show hoga turant url me /about hit krte hi.
+  console.log("middleware chala");
+  // Condition 1:- url ke page pe pahuchne se pahle display pe return kr dega turant hi url me /about hit krte hi.
   // return NextResponse.json({message: 'Hello from the about Page'})
 
   // Condition 2:- url me /about krte hi home page pe leke jaega.. aur url home ho jaega.
-  // return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/", request.url));
   
   // Condition 3: url me /about hit krte home wale page open hoga but url me /about hi rhega
   // return NextResponse.rewrite(new URL("/", request.url));
 };
 
+// See "Matching Paths" below to learn more
 export const config = {
   matcher: "/about/:path*",
 };
@@ -20,14 +21,14 @@ export const config = {
 */
 
 /*********************************************************************/
-import { NextResponse } from 'next/server'
+import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  if (request.nextUrl.pathname.startsWith('/about')) {
-    return NextResponse.rewrite(new URL('/', request.url))
+  if (request.nextUrl.pathname.startsWith("/about")) {
+    return NextResponse.rewrite(new URL("/", request.url));
   }
 
-  if (request.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.rewrite(new URL('/', request.url))
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.rewrite(new URL("/", request.url));
   }
-};
+}
