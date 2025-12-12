8933f15c8a22a69fb6da736863f9a76924462000
Ankit Tripathi <ankittripathi2k24@gmail.com>
2025-12-12 19:37:27 +0530

    Nextjs Course

---
+++ 11_font-optimization/app/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Home Page",
+  description: "font optimization in nextjs",
+};
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
